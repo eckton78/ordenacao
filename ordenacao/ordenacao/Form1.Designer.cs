362b0432e1d2290326534c0a149d73264cd8e8c9
eckton78 <43484609+eckton78@users.noreply.github.com>
2018-12-01 17:23:42 -0200

    Add files via upload

---
+++ ordenacao/ordenacao/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace ordenacao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblAtemp = new System.Windows.Forms.Label();
+            this.lblAfim = new System.Windows.Forms.Label();
+            this.lblAini = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblBtemp = new System.Windows.Forms.Label();
+            this.lblBfim = new System.Windows.Forms.Label();
+            this.lblBini = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblCtemp = new System.Windows.Forms.Label();
+            this.lblCfim = new System.Windows.Forms.Label();
+            this.lblCini = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(6, 11);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(78, 31);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Bubble";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(90, 11);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(78, 31);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Selection";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(174, 11);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(78, 31);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Insertion";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(258, 11);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(78, 31);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "Quick";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(342, 11);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(78, 31);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "Merge";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblAtemp);
+            this.groupBox1.Controls.Add(this.lblAfim);
+            this.groupBox1.Controls.Add(this.lblAini);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 112);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(127, 143);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vetor A";
+            // 
+            // lblAtemp
+            // 
+            this.lblAtemp.AutoSize = true;
+            this.lblAtemp.Location = new System.Drawing.Point(56, 75);
+            this.lblAtemp.Name = "lblAtemp";
+            this.lblAtemp.Size = new System.Drawing.Size(13, 13);
+            this.lblAtemp.TabIndex = 5;
+            this.lblAtemp.Text = "--";
+            // 
+            // lblAfim
+            // 
+            this.lblAfim.AutoSize = true;
+            this.lblAfim.Location = new System.Drawing.Point(56, 62);
+            this.lblAfim.Name = "lblAfim";
+            this.lblAfim.Size = new System.Drawing.Size(13, 13);
+            this.lblAfim.TabIndex = 4;
+            this.lblAfim.Text = "--";
+            // 
+            // lblAini
+            // 
+            this.lblAini.AutoSize = true;
+            this.lblAini.Location = new System.Drawing.Point(56, 47);
+            this.lblAini.Name = "lblAini";
+            this.lblAini.Size = new System.Drawing.Size(13, 13);
+            this.lblAini.TabIndex = 3;
+            this.lblAini.Text = "--";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tempo :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fim:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inicio:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblBtemp);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.lblBfim);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.lblBini);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(149, 112);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(127, 143);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "VetorB";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblCtemp);
+            this.groupBox3.Controls.Add(this.label14);
+            this.groupBox3.Controls.Add(this.lblCfim);
+            this.groupBox3.Controls.Add(this.label15);
+            this.groupBox3.Controls.Add(this.lblCini);
+            this.groupBox3.Controls.Add(this.label13);
+            this.groupBox3.Location = new System.Drawing.Point(293, 112);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(127, 143);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Vetor C";
+            // 
+            // lblBtemp
+            // 
+            this.lblBtemp.AutoSize = true;
+            this.lblBtemp.Location = new System.Drawing.Point(56, 75);
+            this.lblBtemp.Name = "lblBtemp";
+            this.lblBtemp.Size = new System.Drawing.Size(13, 13);
+            this.lblBtemp.TabIndex = 11;
+            this.lblBtemp.Text = "--";
+            // 
+            // lblBfim
+            // 
+            this.lblBfim.AutoSize = true;
+            this.lblBfim.Location = new System.Drawing.Point(56, 62);
+            this.lblBfim.Name = "lblBfim";
+            this.lblBfim.Size = new System.Drawing.Size(13, 13);
+            this.lblBfim.TabIndex = 10;
+            this.lblBfim.Text = "--";
+            // 
+            // lblBini
+            // 
+            this.lblBini.AutoSize = true;
+            this.lblBini.Location = new System.Drawing.Point(56, 47);
+            this.lblBini.Name = "lblBini";
+            this.lblBini.Size = new System.Drawing.Size(13, 13);
+            this.lblBini.TabIndex = 9;
+            this.lblBini.Text = "--";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Tempo :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Fim:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 47);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Inicio:";
+            // 
+            // lblCtemp
+            // 
+            this.lblCtemp.AutoSize = true;
+            this.lblCtemp.Location = new System.Drawing.Point(56, 75);
+            this.lblCtemp.Name = "lblCtemp";
+            this.lblCtemp.Size = new System.Drawing.Size(13, 13);
+            this.lblCtemp.TabIndex = 17;
+            this.lblCtemp.Text = "--";
+            // 
+            // lblCfim
+            // 
+            this.lblCfim.AutoSize = true;
+            this.lblCfim.Location = new System.Drawing.Point(56, 62);
+            this.lblCfim.Name = "lblCfim";
+            this.lblCfim.Size = new System.Drawing.Size(13, 13);
+            this.lblCfim.TabIndex = 16;
+            this.lblCfim.Text = "--";
+            // 
+            // lblCini
+            // 
+            this.lblCini.AutoSize = true;
+            this.lblCini.Location = new System.Drawing.Point(56, 47);
+            this.lblCini.Name = "lblCini";
+            this.lblCini.Size = new System.Drawing.Size(13, 13);
+            this.lblCini.TabIndex = 15;
+            this.lblCini.Text = "--";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(6, 75);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(46, 13);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "Tempo :";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(6, 62);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(26, 13);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "Fim:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(6, 47);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(35, 13);
+            this.label15.TabIndex = 12;
+            this.label15.Text = "Inicio:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 276);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Metodos de Ordenacao";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblAtemp;
+        private System.Windows.Forms.Label lblAfim;
+        private System.Windows.Forms.Label lblAini;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblBtemp;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblBfim;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblBini;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblCtemp;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label lblCfim;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label lblCini;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ ordenacao/ordenacao/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ordenacao
+{
+    public partial class Form1 : Form
+    {
+
+        #region metodos
+
+        public static int[] bubbleSort(int[] vetor)
+        {
+            int tamanho = vetor.Length;
+            int comparacoes = 0;
+            int trocas = 0;
+
+            for (int i = tamanho - 1; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    comparacoes++;
+                    if (vetor[j] > vetor[j + 1])
+                    {
+                        int aux = vetor[j];
+                        vetor[j] = vetor[j + 1];
+                        vetor[j + 1] = aux;
+                        trocas++;
+                    }
+                }
+            }
+
+            return vetor;
+        }
+
+        public static int[] selectionSort(int[] vetor)
+        {
+            int min, aux;
+
+            for (int i = 0; i < vetor.Length - 1; i++)
+            {
+                min = i;
+
+                for (int j = i + 1; j < vetor.Length; j++)
+                    if (vetor[j] < vetor[min])
+                        min = j;
+
+                if (min != i)
+                {
+                    aux = vetor[min];
+                    vetor[min] = vetor[i];
+                    vetor[i] = aux;
+                }
+            }
+
+            return vetor;
+        }
+
+        public static int[] insertionSort(int[] vetor)
+        {
+            int i, j, atual;
+            for (i = 1; i < vetor.Length; i++)
+            {
+                atual = vetor[i];
+                j = i;
+                while ((j > 0) && (vetor[j - 1] > atual))
+                {
+                    vetor[j] = vetor[j - 1];
+                    j = j - 1;
+                }
+                vetor[j] = atual;
+            }
+            return vetor;
+        }
+
+        public static int[] quickSort(int[] vetor)
+        {
+            int inicio = 0;
+            int fim = vetor.Length - 1;
+
+            quickSort1(vetor, inicio, fim);
+
+            return vetor;
+        }
+
+        private static void quickSort1(int[] vetor, int inicio, int fim)
+        {
+            if (inicio < fim)
+            {
+                int p = vetor[inicio];
+                int i = inicio + 1;
+                int f = fim;
+
+                while (i <= f)
+                {
+                    if (vetor[i] <= p)
+                    {
+                        i++;
+                    }
+                    else if (p < vetor[f])
+                    {
+                        f--;
+                    }
+                    else
+                    {
+                        int troca = vetor[i];
+                        vetor[i] = vetor[f];
+                        vetor[f] = troca;
+                        i++;
+                        f--;
+                    }
+                }
+
+                vetor[inicio] = vetor[f];
+                vetor[f] = p;
+
+                quickSort1(vetor, inicio, f - 1);
+                quickSort1(vetor, f + 1, fim);
+            }
+        }
+
+           private static List<int> MergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+                return unsorted;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            int middle = unsorted.Count / 2;
+            for (int i = 0; i < middle;i++)  //Dividing the unsorted list
+            {
+                left.Add(unsorted[i]);
+            }
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while(left.Count > 0 || right.Count>0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())  
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());      
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if(left.Count>0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+
+                    right.Remove(right.First());    
+                }    
+            }
+            return result;
+        }
+
+        
+
+        #endregion
+
+        List<int> vetA = new List<int>();
+        List<int> vetB = new List<int>();
+        List<int> vetC = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Novo();
+
+        }
+
+        public void Novo()
+        {
+            vetA.Clear();
+            vetB.Clear();
+            vetC.Clear();
+
+            preencherA();
+            preencherB();
+            preencherC();
+
+            lblAini.Text = "";
+            lblAfim.Text = "";
+            lblAtemp.Text = "";
+
+            lblBini.Text = "";
+            lblBfim.Text = "";
+            lblBtemp.Text = "";
+
+            lblCini.Text = "";
+            lblCfim.Text = "";
+            lblCtemp.Text = "";
+
+            return;
+        }
+
+        public void preencherA()
+        {
+            for (int i = 1; i <= 100000; i++)
+            {
+                vetA.Add(i);
+            }
+        }
+
+        public void preencherB()
+        {
+            for (int i = 100000; i >= 1; i--)
+            {
+                vetB.Add(i);
+            }
+        }
+
+        public void preencherC()
+        {
+            Random r = new Random();
+
+            for (int i = 1; i <= 100000; i++)
+            {
+                vetC.Add(r.Next(0, 9));
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Novo();
+
+            var aux1A = lblAini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            bubbleSort(vetA.ToArray());
+            var aux2A = lblAfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblAtemp.Text = Convert.ToString(Convert.ToInt32(aux2A) - Convert.ToInt32(aux1A));
+
+            var aux1B = lblBini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            bubbleSort(vetB.ToArray());
+            var aux2B = lblBfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblBtemp.Text = Convert.ToString(Convert.ToInt32(aux2B) - Convert.ToInt32(aux1B));
+
+            var aux1C = lblCini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            bubbleSort(vetC.ToArray());
+            var aux2C = lblCfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblCtemp.Text = Convert.ToString(Convert.ToInt32(aux2C) - Convert.ToInt32(aux1C));
+
+            return;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Novo();
+
+            var aux1A = lblAini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            selectionSort(vetA.ToArray());
+            var aux2A = lblAfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblAtemp.Text = Convert.ToString(Convert.ToInt32(aux2A) - Convert.ToInt32(aux1A));
+
+            var aux1B = lblBini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            selectionSort(vetB.ToArray());
+            var aux2B = lblBfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblBtemp.Text = Convert.ToString(Convert.ToInt32(aux2B) - Convert.ToInt32(aux1B));
+
+            var aux1C = lblCini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            selectionSort(vetC.ToArray());
+            var aux2C = lblCfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblCtemp.Text = Convert.ToString(Convert.ToInt32(aux2C) - Convert.ToInt32(aux1C));
+
+            return;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            Novo();
+
+            var aux1A = lblAini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            insertionSort(vetA.ToArray());
+            var aux2A = lblAfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblAtemp.Text = Convert.ToString(Convert.ToInt32(aux2A) - Convert.ToInt32(aux1A));
+
+            var aux1B = lblBini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            insertionSort(vetB.ToArray());
+            var aux2B = lblBfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblBtemp.Text = Convert.ToString(Convert.ToInt32(aux2B) - Convert.ToInt32(aux1B));
+
+            var aux1C = lblCini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            insertionSort(vetC.ToArray());
+            var aux2C = lblCfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblCtemp.Text = Convert.ToString(Convert.ToInt32(aux2C) - Convert.ToInt32(aux1C));
+
+            return;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            Novo();
+
+            var aux1A = lblAini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            quickSort(vetA.ToArray());
+            var aux2A = lblAfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblAtemp.Text = Convert.ToString(Convert.ToInt32(aux2A) - Convert.ToInt32(aux1A));
+
+            var aux1B = lblBini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            quickSort(vetB.ToArray());
+            var aux2B = lblBfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblBtemp.Text = Convert.ToString(Convert.ToInt32(aux2B) - Convert.ToInt32(aux1B));
+
+            var aux1C = lblCini.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            quickSort(vetC.ToArray());
+            var aux2C = lblCfim.Text = DateTime.Now.TimeOfDay.Milliseconds.ToString();
+            lblCtemp.Text = Convert.ToString(Convert.ToInt32(aux2C) - Convert.ToInt32(aux1C));
+
+            return;
+        }
+    }
+}
